9726ac27561c3fa81283be26bd67a06cc5be0898
korsakowii <yanxiaoy@usc.edu>
2021-06-30 18:31:54 -0700

    Update images and fonts

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,8 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-    { id: 1, name: 'Air Purifier', description: 'Air purifier, used - good', price: '$50' },
-    { id: 2, name: 'Shelf', description: 'Steel shelf, opened but never used', price: '$5' },
+    { id: 1, name: 'Air Purifier', description: 'Air purifier, used twice', price: '$50', image: 'https://images-na.ssl-images-amazon.com/images/I/81ZOioO6M4L._AC_SL1500_.jpg' },
+    { id: 2, name: 'Shelf', description: 'Steel shelf, opened but never used', price: '$5', image: 'https://www.ikea.com/us/en/images/products/omar-1-section-shelving-unit__0912296_pe691230_s5.jpg?f=g' },
 ];
 
 const Products = () => {
