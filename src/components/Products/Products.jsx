f2c218e791fbe42b8836f72d2443ba4b9731e891
korsakowii <yanxiaoy@usc.edu>
2021-07-03 00:32:17 -0700

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Air Purifier', description: 'Air purifier, used twice', price: '$50', image: 'https://images-na.ssl-images-amazon.com/images/I/81ZOioO6M4L._AC_SL1500_.jpg' },
-    { id: 2, name: 'Shelf', description: 'Steel shelf, opened but never used', price: '$5', image: 'https://www.ikea.com/us/en/images/products/omar-1-section-shelving-unit__0912296_pe691230_s5.jpg?f=g' },
+    { id: 2, name: 'Shelf', description: 'Steel shelf, opened but never used', price: '$5', image: 'https://www.ikea.com/us/en/images/products/omar-shelf-unit-galvanized__0626610_ph124828_s5.jpg?f=xl' },
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
