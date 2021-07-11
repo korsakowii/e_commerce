aefd86f4a831e10dee07a138318c96b37abfeaa9
korsakowii <yanxiaoy@usc.edu>
2021-07-11 13:47:50 -0700

    Implement React router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
@@ -12,13 +13,14 @@ const Navbar = ({ totalItems }) => {
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
                         UBN
                     </Typography>
                     <div className={classes.grow} />
                     <div className={classes.button}>
-                        <IconButton aria-label="Shop cart items" color="inherit">
+                        <Link to="/cart">Go to Cart</Link>
+                        <IconButton component={Link} to="/cart" aria-label="Shop cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
