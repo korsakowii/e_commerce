30c26ad021dd520f36f6df13d1a584bfc6171c54
korsakowii <yanxiaoy@usc.edu>
2021-08-08 16:53:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
-    const [order, setOrder] = useState([]);
-    const [errorMessage, setErrorMessage] = useState([]);
+    const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
